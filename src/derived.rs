0e0f516925171933301de9fe93e111bbae828be0
Kunta-tech <kundusaugata576@gmail.com>
2025-07-19 02:19:53 +0530

    Inject

---
+++ src/derived.rs
@@ -0,0 +1,26 @@
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+
+pub fn derive_hooks_impl(input: TokenStream) -> TokenStream {
+    // Parse the input into a syntax tree
+    let ast = parse_macro_input!(input as DeriveInput);
+
+    // Extract the struct name
+    let name = &ast.ident;
+
+    // Generate the impl Hooks
+    let expanded = quote! {
+        impl Hookable for #name {
+            fn run_hooks(&self){
+                self.startup();
+                self.run();
+                self.cleanup();
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
